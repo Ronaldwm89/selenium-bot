f16bebfc99ae61c8ef850f5158290b633f9b78e8
Ronald <ronaldwmm1989@gmail.com>
2025-08-04 20:19:09 -0300

    working in the project automatization playing clicker cookie

--- main.py
+++ main.py
@@ -10,15 +10,28 @@ crhome_options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options = crhome_options)
 driver.get('https://ozh.github.io/cookieclicker')
 
-time.sleep(5)  # Wait for the page to load
+time.sleep(2)  # Wait for the page to load
 language = driver.find_element(By.XPATH, '//*[@id="langSelect-ES"]')
 language.click()
 
-time.sleep(5)
-
-inicio = time.time()# Wait for the language to change
-while time.time() - inicio < 20:
-    cookie = driver.find_element(By.CSS_SELECTOR, '#bigCookie')
+time.sleep(3) #wait for the cookie to appear
+five_seconds = time.time()
+inicio = time.time() #inicio del tiempo de ejecucion
+while time.time() - inicio < 40:
+    cookie = driver.find_element(By.CSS_SELECTOR, '#bigCookie')# Find the cookie element and click it
     cookie.click()
+    
+    
+    if time.time() - five_seconds > 5:
+        prices = driver.find_elements(By.CSS_SELECTOR, '.price')
+        
+        for price in prices:
+            print(price.text)
+        
+        print('acaban de pasar 5 segundos')#
+        five_seconds = time.time()
+        
+    
    
 
+print(time.time())
